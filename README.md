fb61e3981b9522e8c7b3af1ef7146593a34ca36b
umutyaldiz <umutyaldiz95@gmail.com>
2021-09-20 13:38:25 +0300

    webpack version ++

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Infinite Scroll JAVASCRIPT
 Automatically add next page
 
 # install
-"npm install"  ==> "npm run build" ==> "/dev" files  | Congratulations! :)
+"npm install"  ==> "npm run (dev or prod)" ==> "/dev" files  | Congratulations! :)
 
 
---
+++ webpack.dev.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+module.exports = {
+    entry: {
+        app: './src/infinite.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dev'),
+        filename: 'infinite_bundle.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [
+                            "@babel/plugin-proposal-class-properties"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+};
+
